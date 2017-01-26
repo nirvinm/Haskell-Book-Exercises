3a2532b7c88fc091c7f9c5162065f35ac24d2fd6
Nirvin M <nirvin@outlook.com>
2017-01-26 20:50:49 +0530

    Solving capitalizeWords exercise

--- Algebraic_DataTypes_Chapter_11/Chapter11.hs
+++ Algebraic_DataTypes_Chapter_11/Chapter11.hs
@@ -1,10 +1,11 @@
 module Chapter11 where 
 
+import Data.Char
+
 -- Use as-patterns in implementing the following functions:
 -- 1. This should return True if (and only if) all the values in the
 --    first list appear in the second list, though they need not be
 --    contiguous.
-
 isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf [] _ = True
 isSubsequenceOf ks [] = False
@@ -12,3 +13,12 @@ isSubsequenceOf ks@(k:ks') xs@(x:xs') =
     if k == x
     then isSubsequenceOf ks' xs'
     else isSubsequenceOf ks  xs'
+
+-- 2. Split a sentence into words, then tuple each word with the capi-
+--    talized form of each.
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords xs = map (\x -> (x, capitalizeWord x)) $ words xs
+
+capitalizeWord :: String -> String
+capitalizeWord [] = []
+capitalizeWord (x:xs) = (toUpper x) : xs
