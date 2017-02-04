38dd0235b21992fc64af5c9bbd3008f237f39b54
Nirvin M <nirvin@outlook.com>
2017-02-04 21:58:10 +0530

    Adding solution to capitalizeParagraph problem

--- Chapter_11_Algebraic_DataTypes/Chapter11.hs
+++ Chapter_11_Algebraic_DataTypes/Chapter11.hs
@@ -1,6 +1,7 @@
 module Chapter11 where 
 
 import Data.Char
+import Data.List ( intercalate )
 
 -- Use as-patterns in implementing the following functions:
 -- 1. This should return True if (and only if) all the values in the
@@ -22,3 +23,21 @@ capitalizeWords xs = map (\x -> (x, capitalizeWord x)) $ words xs
 capitalizeWord :: String -> String
 capitalizeWord [] = []
 capitalizeWord (x:xs) = (toUpper x) : xs
+
+-- Write a function that capitalizes sentences in a paragraph.
+-- Recognize when a new sentence has begun by checking for periods.
+-- Reuse the capitalizeWord function.
+capitalizeParagraph :: String -> String
+capitalizeParagraph p = intercalate ". " $ map (capitalizeWord.trim) $ split '.' p
+
+trim :: String -> String
+trim = unwords.words
+
+split :: Char -> String -> [String]
+split delimiter sentence =
+    -- ws = processed words
+    -- w  = current word
+    let accumulate c (ws, w) = if c == delimiter then (w:ws, []) else (ws, c:w)
+        reduce xs = foldr accumulate ([], []) xs
+        result = reduce sentence
+    in (snd result):(fst result)
