4db743c4e30ae2350b4ddba32fabf9599fa34d7e
Nirvin M <nirvin@outlook.com>
2017-04-15 13:28:20 +0530

    Added reference to original paper

--- Chapter_11_Algebraic_DataTypes/huttons_razor.hs
+++ Chapter_11_Algebraic_DataTypes/huttons_razor.hs
@@ -1,4 +1,4 @@
--- Hutton’s Razor 9 is a very simple expression language that expresses
+-- Hutton’s Razor is a very simple expression language that expresses
 -- integer literals and addition of values in that expression language.
 -- The “trick” to it is that it’s recursive and the two expressions you’re
 -- summing together could be literals or themselves further addition
@@ -6,6 +6,9 @@
 -- guages used to motivate ideas in research papers and functional
 -- pearls. Evaluating or folding a datatype is also in some sense what
 -- you’re doing most of the time while programming anyway.
+
+
+-- Inspired from the paper: http://www.cs.nott.ac.uk/~pszgmh/bib.html#semantics
 
 -- 1. Your first task is to write the “eval” function which reduces an
 -- expression to a final sum.
