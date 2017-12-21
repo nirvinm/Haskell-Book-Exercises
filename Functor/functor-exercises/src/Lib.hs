b46ea816204201195ecf521ea9428ffce6e1423a
Nirvin M <nirvin@outlook.com>
2017-12-21 18:25:22 +0530

    Solving few more exercises from Functor chapter

--- Functor/functor-exercises/src/Lib.hs
+++ Functor/functor-exercises/src/Lib.hs
@@ -1,7 +1,31 @@
 module Lib where
 
-newtype Identity a = Identity a
+newtype Identity a = Identity a deriving (Eq, Show)
 
 instance Functor Identity  where
     fmap f (Identity a) = Identity (f a)
 
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+    fmap f (Pair a b) = Pair (f a) (f b)
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+    fmap f (Two a b) = Two a $ f b
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+    fmap f (Three a b c) = Three a b $ f c
+
+data Three' a b = Three' a b b deriving (Eq, Show)
+
+instance Functor (Three' a) where
+    fmap f (Three' a b b') = Three' a (f b) (f b')
+
+-- Can you implement one for this type? Why? Why not?
+--   Since the kind of this type is * and not * -> *,
+--   a Functor instance cannot be defined.
+data Trivial = Trivial
--- Functor/functor-exercises/test/Spec.hs
+++ Functor/functor-exercises/test/Spec.hs
@@ -16,7 +16,92 @@ functorCompose' x (Fun _ f) (Fun _ g) = (fmap (g . f) x) == (fmap g . fmap f $ x
 type IntToInt = Fun Int Int
 type IntFC = [Int] -> IntToInt -> IntToInt -> Bool
 
+-- Exercises: Instances of Func
+-- Implement Functor instances for the following datatypes. Use the
+-- QuickCheck properties we showed you to validate them.
+
+-- Tests for Identity
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+    arbitrary = do
+        a <- arbitrary        
+        return $ Identity a        
+
+type IdentityFC = Identity Int -> IntToInt -> IntToInt -> Bool
+
+testIdentity = do
+    quickCheck (functorIdentity :: (Identity Int) -> Bool)
+    quickCheck (functorCompose' :: IdentityFC)
+
+-- Tests for Pair 
+instance (Arbitrary a) => Arbitrary (Pair a) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        return $ Pair a b
+
+type PairFC = Pair Int -> IntToInt -> IntToInt -> Bool
+
+testPair = do
+    quickCheck (functorIdentity :: (Pair Int) -> Bool)
+    quickCheck (functorCompose' :: PairFC)
+
+
+
+    -- Test for Two
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        return $ Two a b
+
+type TwoFC = Two Int Int -> IntToInt -> IntToInt -> Bool
+
+testTwo = do
+    quickCheck (functorIdentity :: (Two Int Int) -> Bool)
+    quickCheck (functorCompose' :: TwoFC)
+
+
+
+-- Test for Three
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        c <- arbitrary
+        return $ Three a b c
+
+type ThreeFC = Three Int Int Int -> IntToInt -> IntToInt -> Bool
+
+testThree = do
+    quickCheck (functorIdentity :: (Three Int Int Int) -> Bool)
+    quickCheck (functorCompose' :: ThreeFC)
+
+
+
+-- Test for Three'
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        b' <- arbitrary
+        return $ Three' a b b'
+
+type Three'FC = Three' Int Int -> IntToInt -> IntToInt -> Bool
+
+testThree' = do
+    quickCheck (functorIdentity :: (Three' Int Int) -> Bool)
+    quickCheck (functorCompose' :: Three'FC)
+
+
+
 main :: IO ()
 main = do
     quickCheck (functorIdentity :: [Int] -> Bool)
     quickCheck (functorCompose' :: IntFC)
+    testIdentity
+    testPair
+    testTwo
+    testThree
+    testThree'
+
