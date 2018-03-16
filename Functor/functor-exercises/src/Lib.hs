20e2bd5a56e6c77ccd7f6c4c517481b5e831550f
Nirvin M <nirvin@outlook.com>
2018-03-16 08:22:52 +0530

    Solving few more exercises in Functor chapter

--- Functor/functor-exercises/src/Lib.hs
+++ Functor/functor-exercises/src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Lib where
 
 newtype Identity a = Identity a deriving (Eq, Show)
@@ -29,3 +31,29 @@ instance Functor (Three' a) where
 --   Since the kind of this type is * and not * -> *,
 --   a Functor instance cannot be defined.
 data Trivial = Trivial
+
+
+-- Write a Functor instance for a datatype identical to Maybe. We’ll use
+-- our own datatype because Maybe already has a Functor instance and
+-- we cannot make a duplicate one.
+data Possibly a =
+      LolNope
+    | Yeppers a
+    deriving (Eq, Show)
+
+instance Functor Possibly where
+    fmap f LolNope = LolNope
+    fmap f (Yeppers a) = Yeppers (f a)
+
+-- 1. Write a Functor instance for a datatype identical to Either. We’ll
+--    use our own datatype because Either has a Functor instance.
+data Sum a b =
+      First a
+    | Second b
+    deriving (Eq, Show)
+
+instance Functor (Sum a) where
+    fmap f (First a) = First a
+    fmap f (Second b) = Second (f b)
+
+type Nat f g = forall a . f a -> g a
